06bba4ed333540804604cf022fdf49affdd93f1a
bipul724 <bipulchamoli45@gmail.com>
2026-01-29 10:21:59 +0530

    second commit

---
+++ 29-jan-2026/App.jsx
@@ -0,0 +1,11 @@
+console.log("Hii");
+const parent = document.getElementById("container");
+const root = ReactDOM.createRoot(parent);
+const h2 = React.createElement("h2",{},"Welcome to ReactJS");
+const li1 = React.createElement("li",{},"Java programming");
+const li2 = React.createElement("li",{},"C++ programming");
+const ul = React.createElement("ul",{},[li1,li2]);
+const div = React.createElement("div",{},[h2,ul]);
+
+root.render(div);
+
