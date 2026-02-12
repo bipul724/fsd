309d09cf9701f26842960484b84db561a4d917c2
bipul724 <bipulchamoli45@gmail.com>
2026-02-12 09:37:57 +0530

    Added promise.js

---
+++ 12-feb-2026/promise.js
@@ -0,0 +1,15 @@
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const success = Math.random() > 0.5;
+
+        if (success) {
+            resolve('Operation completed successfully');
+        } else {
+            reject(new Error('Operation failed'));
+        }
+    }, 1000);
+});
+
+myPromise
+    .then(result => console.log(result))
+    .catch(error => console.error(error.message));
